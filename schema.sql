fb7e45395c9cf9f0bc88f91867d5b35432e5cb5b
Abdullah <abdullahkhrief@gmail.com>
2023-06-15 16:21:05 +0300

    Have added the tables'

--- schema.sql
+++ schema.sql
@@ -15,12 +15,12 @@ ADD species VARCHAR(25);
 
 BEGIN;
 CREATE TABLE owners(
-    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    id INT GENERATED ALWAYS AS SERIAL PRIMARY KEY,
     full_name VARCHAR(25),
     age INT
 );
 CREATE TABLE species(
-    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    id INT GENERATED ALWAYS AS SERIAL PRIMARY KEY,
     name VARCHAR(25)
 );
 
@@ -39,3 +39,29 @@ ALTER TABLE animals
 ADD owners_id INT,
 ADD FOREIGN KEY (owners_id)
 REFERENCES  owners(id);
+
+BEGIN;
+CREATE TABLE vets (
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name VARCHAR(25),
+    age INT,
+    date_of_graduation DATE
+);
+
+CREATE TABLE specializations (
+    species INT,
+    vets INT,
+    PRIMARY KEY (species,vets),
+    FOREIGN KEY (species) REFERENCES species(id),
+    FOREIGN KEY (vets) REFERENCES vets(id)
+);
+
+CREATE TABLE visits (
+    animals INT,
+    vets INT,  
+    PRIMARY KEY (animals, vets),
+    FOREIGN KEY (animals) REFERENCES animals(id),
+    FOREIGN KEY (vets) REFERENCES vets(id)
+);
+
+END;
